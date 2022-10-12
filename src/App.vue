1c286d493351f3bd1fadf6e1d1bf68ae0ddfeeeb
Fkmog <1073638314@qq.com>
2022-10-12 15:34:21 +0800

    handsometable v0.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <hot-table :data="data" :rowHeaders="true" :colHeaders="true" :licenseKey="licenseKey"></hot-table>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import { HotTable } from '@handsontable/vue3';
+import 'handsontable/dist/handsontable.full.css';//解决样式没有的问题
+import { registerAllModules } from 'handsontable/registry';
+
+
+registerAllModules();
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HotTable
+  },
+data(){
+  return{
+    data:[
+    ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+          ['2016', 10, 11, 12, 13],
+          ['2017', 20, 11, 14, 13],
+          ['2018', 30, 15, 12, 13]
+    ],
+    licenseKey:'non-commercial-and-evaluation'
+  };
+},
 }
+
+
+
+
+
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
