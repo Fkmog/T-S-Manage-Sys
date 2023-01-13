65e0e3d5be04172ed852b137aa3bc52c214cb3be
Fkmog <1073638314@qq.com>
2023-01-13 14:55:15 +0800

    1.13 dyf

--- src/store/index.js
+++ src/store/index.js
@@ -108,17 +108,6 @@ export default createStore({
       state: {
         courseName:'',
         detailId:Number,
-        baseCourseVersionName:'',
-        baseCourseVersionFlag:'',
-        baseCourseVersionId:'',
-        baseCourseCourseId:'',
-        baseCourseCourseName: '',
-        baseCourseCourseCode: '',
-        baseCourseCourseType: '',
-        baseCourseCourseNature: '',
-        baseCourseCredit: '',
-        baseCourseCourseYear: '',
-        baseCourseRemark: '',
       },
       getters: {},
       mutations: {
@@ -127,39 +116,6 @@ export default createStore({
         },
         setDetailId(state, newVal) {
           state.detailId = newVal
-        },
-        setbaseCourseVersionName(state, newVal) {
-          state.baseCourseVersionName = newVal
-        },
-        setbaseCourseVersionFlag(state, newVal) {
-          state.baseCourseVersionFlag = newVal
-        },
-        setbaseCourseVersionId(state, newVal) {
-          state.baseCourseVersionId = newVal
-        },
-        setbaseCourseCourseId(state, newVal) {
-          state.baseCourseCourseId = newVal
-        },
-        setbaseCourseCourseName(state, newVal) {
-          state.baseCourseCourseName = newVal
-        },
-        setbaseCourseCourseCode(state, newVal) {
-          state.baseCourseCourseCode = newVal
-        },
-        setbaseCourseCourseType(state, newVal) {
-          state.baseCourseCourseType = newVal
-        },
-        setbaseCourseCourseNature(state, newVal) {
-          state.baseCourseCourseNature = newVal
-        },
-        setbaseCourseCredit(state, newVal) {
-          state.baseCourseCredit = newVal
-        },
-        setbaseCourseCourseYear(state, newVal) {
-          state.baseCourseCourseYear = newVal
-        },
-        setbaseCourseRemark(state, newVal) {
-          state.baseCourseRemark = newVal
         },
       },
       actions: {},
