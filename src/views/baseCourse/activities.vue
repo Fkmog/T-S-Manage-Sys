599c9022b2cbd3de0238e5d66fc0384c4d2e585d
Fkmog <1073638314@qq.com>
2024-02-04 14:08:32 +0800

    yy 0204 xiawu

--- src/views/baseCourse/activities.vue
+++ src/views/baseCourse/activities.vue
@@ -1121,7 +1121,7 @@ export default {
                 if (res.code == "SUCCESS") {
                   ElMessage({
                     type: "success",
-                    message: `更新成功`,
+                    message: `保存成功`,
                     duration: 1500,
                   });
                 }
